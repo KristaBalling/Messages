5aafa0d4fb2c55d8d503818ef7e7ebf7dd5152d4
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-22 10:01:37 -0500

    deadlocks, wait, notify and notifyAll

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,6 +5,9 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
 
     }
 }
@@ -15,18 +18,29 @@ class Message {
 
     public synchronized String read() {
         while(empty) {
+            try {
+                wait();
+            } catch(InterruptedException e) {
+
+            }
 
         }
         empty = true;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
         while (!empty) {
+            try {
+                wait();
+            } catch(InterruptedException e) {
+            }
 
         }
         empty = false;
         this.message = message;
+        notifyAll();
     }
 }
 
@@ -49,6 +63,28 @@ class Writer implements Runnable {
 
         for(int i=0; i<messages.length; i++) {
             message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch(InterruptedException e) {
+
+            }
+        }
+        message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for(String latestMessage = message.read(); !latestMessage.equals("Finished");
+            latestMessage = message.read()) {
+            System.out.println(latestMessage);
             try {
                 Thread.sleep(random.nextInt(2000));
             } catch(InterruptedException e) {
